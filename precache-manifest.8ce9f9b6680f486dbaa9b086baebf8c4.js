9772ad6c237ced5088efdc34fdce1d2160ab7ff0
Selahadin Jemal <sjemal@jsi.local>
2024-06-06 19:13:45 +0300

    Updates

---
+++ precache-manifest.8ce9f9b6680f486dbaa9b086baebf8c4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7a3014079d82189e9be2050d701ac6",
+    "url": "/telgram-react/index.html"
+  },
+  {
+    "revision": "7935d0581b2e05d2815e",
+    "url": "/telgram-react/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "776a59d1f6709db58830",
+    "url": "/telgram-react/static/css/4.65536c90.chunk.css"
+  },
+  {
+    "revision": "8d876672a9ebd201e992",
+    "url": "/telgram-react/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "8a949a2cf2d448b987b2",
+    "url": "/telgram-react/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "74ca294df5a15d18a80e",
+    "url": "/telgram-react/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "7935d0581b2e05d2815e",
+    "url": "/telgram-react/static/js/0.e6a535e0.chunk.js"
+  },
+  {
+    "revision": "f16dcd99a1d8edd3b4f0",
+    "url": "/telgram-react/static/js/3.741b7fe9.chunk.js"
+  },
+  {
+    "revision": "776a59d1f6709db58830",
+    "url": "/telgram-react/static/js/4.93c2837b.chunk.js"
+  },
+  {
+    "revision": "8d876672a9ebd201e992",
+    "url": "/telgram-react/static/js/5.a98ab2e9.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/telgram-react/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "8a949a2cf2d448b987b2",
+    "url": "/telgram-react/static/js/7.caa38765.chunk.js"
+  },
+  {
+    "revision": "74ca294df5a15d18a80e",
+    "url": "/telgram-react/static/js/main.b99efb06.chunk.js"
+  },
+  {
+    "revision": "3a049a528503eea37397",
+    "url": "/telgram-react/static/js/runtime~main.0c67bb69.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/telgram-react/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telgram-react/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telgram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telgram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
